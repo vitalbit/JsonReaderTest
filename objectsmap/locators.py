781fc605e9b4a12a6e4ca73fecee8489f46ad89a
Vitalii <vitalbit@tut.by>
2015-02-24 11:28:41 +0300

    objectsmap class login

---
+++ objectsmap/locators.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+
+class Login:
+    Enter_Menu = (By.ID, 'enter')
+    Login_Text = (By.ID, 'login')
+    Password_Text = (By.ID, 'password')
+    Submit_Button = (By.ID, 'subm_button')
+    Exit_Menu = (By.ID, 'exit')
+
+class JsonTree:
+    
